19e1fdbbd6671a101eb1da62ea025e117e769c73
VelesW <michalwilk296@gmail.com>
2024-11-07 12:09:33 +0100

    Create basic resumeGame

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const  scaleFactor = 4;
+export const dialogueData = {
+    pc: `This is my PC. I spend most of my time here. I'm a computer science student, so I have to work on a lot of projects. I also play games and watch movies on it.`,
+  "cs-degree": `I'm currently pursuing a Bachelor's degree in Computer Science. I'm in my final year, and I'm looking for opportunities to work as a software developer.`,
+  "sofa-table": `This is my sofa. I usually sit here when I'm not working. I watch TV shows and movies here. I also read books and play games on my phone.`,
+  tv: `This is my TV. I watch TV shows and movies on it. I also play games on my PS4. I have a collection of Blu-ray movies and TV shows.`,
+  bed: `This is my bed. I sleep here. I also read books and watch movies on my tablet before going to sleep.`,
+  resume: `This is my resume. I have listed my skills, education, and work experience in it. I'm looking for opportunities to work as a software developer.`,
+  projects: `These are some of the projects I have worked on. I have experience in web development, mobile app development, and game development.`,
+  library: `This is my bookshelf. I have a collection of books on programming, data structures, algorithms, and software development. I also have some fiction and non-fiction books.`,
+  exit: `This is the exit. I can leave the room by clicking on it.`,
+};
